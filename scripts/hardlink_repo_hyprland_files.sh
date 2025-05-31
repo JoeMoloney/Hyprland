c54f04a9659280a21f940426f83c758a013838b2
JoeMoloney <joe.moloney496@gmail.com>
2025-06-01 00:17:02 +0100

    added hard symlink creation script

---
+++ scripts/hardlink_repo_hyprland_files.sh
@@ -0,0 +1,4 @@
+sudo ln ~/.config/hypr/hyprland.conf ~/GitRepositories/Hyprland/hyprland.conf
+sudo ln ~/.config/hypr/keybindings/customBindings.conf ~/GitRepositories/Hyprland/keybindings/customBindings.conf
+sudo ln ~/.config/hypr/monitors/monitors.conf ~/GitRepositories/Hyprland/monitors/monitors.conf
+sudo ln ~/.config/hypr/windows/windows.conf ~/GitRepositories/Hyprland/windows/windows.conf
