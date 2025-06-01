e65416a1b65ffc06b0b1e4c6fbf30d657189e9a8
JoeMoloney <joe.moloney496@gmail.com>
2025-06-01 14:35:01 +0100

    update waybar config for customizing

--- scripts/hardlink_repo_hyprland_files.sh
+++ scripts/hardlink_repo_hyprland_files.sh
@@ -3,3 +3,5 @@ sudo ln ~/.config/hypr/keybindings/customBindings.conf ~/GitRepositories/Hyprlan
 sudo ln ~/.config/hypr/monitors/monitors.conf ~/GitRepositories/Hyprland/monitors/monitors.conf
 sudo ln ~/.config/hypr/windows/windows.conf ~/GitRepositories/Hyprland/windows/windows.conf
 sudo ln ~/.config/hypr/keybindings/volumeBindings.conf ~/GitRepositories/Hyprland/keybindings/volumeBindings.conf
+sudo ln ~/.config/waybar/config.jsonc ~/GitRepositories/Hyprland/waybar/config.jsonc
+sudo ln ~/.config/waybar/style.css ~/GitRepositories/Hyprland/waybar/style.css
